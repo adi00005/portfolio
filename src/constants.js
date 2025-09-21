a98b60ab98658fc090228d514db31e2b34269730
Aditya <p4u.intern@gmail.com>
2025-09-21 20:24:58 +0530

    project updated

---
+++ src/constants.js
@@ -0,0 +1,328 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import mssqlLogo from './assets/tech_logo/mssql.png'
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import edurekha from './assets/company_logo/edurekha.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+import p4uLogo from './assets/work_logo/p4u.jpg'
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+import aitm from "./assets/education_logo/aitm.png";
+import maheshpu from "./assets/education_logo/maheshpu.png";
+import shri from "./assets/education_logo/shri.png";
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import p4u from './assets/work_logo/p4u.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import medicinalplantLogo from './assets/work_logo/medicinalplant.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      // { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
+      { name: 'MSSQL', logo: mssqlLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      // { name: 'Vercel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+  id: 1,
+  img: edurekha,
+  role: "Data Science and AI/ML Intern",
+  company: "Eurekha Digital Solutions Pvt Ltd",
+  date: "April 2022 - June 2022",
+  desc: "Worked on real-world data science and AI/ML projects, focusing on data preprocessing, feature engineering, and model development. Implemented machine learning algorithms and built predictive models to extract insights and improve decision-making. Gained hands-on experience with deep learning using Convolutional Neural Networks (CNN) for image classification and participated in deploying models for practical use cases.",
+  skills: [
+    "Python",
+    "Pandas",
+    "NumPy",
+    "Matplotlib",
+    "Scikit-learn",
+    "TensorFlow",
+    "Keras",
+    "Machine Learning",
+    "Deep Learning",
+    "CNN",
+  ],
+}
+,
+    {
+      id: 0,
+      img: p4uLogo,
+      role: "Fullstack Developer",
+      company: "P4U Healthcare Bgm",
+      date: "December 2024 - Present",
+      desc: "Developed dynamic and scalable web applications using the Web Application , handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement  APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "AngularJs",
+        "SCSS",
+        "JavaScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MSSQL"
+      ],
+    },
+    // {
+    //   id: 1,
+    //   img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //     "SQL",
+    //   ],
+    // },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: aitm,
+      school: "Angadi Institute of Technology And Management Belagavi",
+      date: "Sept 2021 - July 2025",
+      grade: "7.6 CGPA",
+      desc: "I am pursuing my Bachelor's degree in Computer Science and Engineering from Angadi Institute of Technology and Management, Belagavi. Throughout my academic journey, I have gained a strong foundation in programming, software development, and core computer science concepts. My coursework includes Data Structures and Algorithms, Operating Systems, Database Management Systems, Computer Networks, Web Development, and Software Engineering. I have actively worked on projects and participated in technical events, which helped me strengthen my problem-solving skills and practical knowledge.",
+  degree: "Bachelor of Engineering - B.E. (Computer Science and Engineering)",
+    },
+    {
+      id: 1,
+      img: maheshpu,
+      school: "Sgv Mahesh PU College,Belgavi",
+      date: "Sept 2018 - Aug 2020",
+      grade: "65%",
+        desc: "I completed my Pre-University Course (PUC) in the PCMB stream (Physics, Chemistry, Mathematics, and Biology) from SGV Mahesh PU College, Belgavi. During this time, I built a strong foundation in core science subjects, developed analytical and problem-solving skills, and cultivated an interest in technology and research. This period helped me strengthen my logical thinking and prepared me for my future studies in computer science.",
+  degree: "PUC - PCMB (Science Stream)",
+
+    },
+    {
+      id: 2,
+      img: shri,
+      school: "Shri Saraswati English Medium School, Ghataprabha, Gokak",
+      date: "Apr 2017 - March 2018",
+      grade: "82%",
+      desc: "I completed my class 10 education from Shri Saraswati English Medium School, Ghataprabha, Gokak, under the Karnataka State board, where I studied SSLC with Science and Mathematics.",
+      degree: "SSLC(X) ",
+    },
+    // {
+    //   id: 3,
+    //   img: vpsLogo,
+    //   school: "Vatsalya Public School Govardhan, Mathura",
+    //   date: "Apr 2015 - March 2016",
+    //   grade: "87.5%",
+    //   desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+    //   degree: "CBSE(X), Science with Computer Application",
+    // },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "P4U Healthcare A Complete Healthcare Platform",
+      description:
+        "A powerful and user-friendly healthcare platform that connects job seekers, professionals, and organizations in the medical industry. Users can create profiles, apply for jobs, and engage with peers through posts, likes, comments, and follows — making P4U Healthcare an essential tool for career growth and professional networking.",      image: p4u,
+      tags: ["AngularJs",
+        "SCSS",
+        "JavaScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MSSQL"],
+      // github: "https://p4uhealthcare.com/dashboard",
+      webapp: "https://p4uhealthcare.com/dashboard",
+    },
+    {
+      id: 1,
+      title: "CS Prep",
+      description:
+        "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+      image: csprepLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/adi00005/CS-Prep",
+      webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/adi00005/Movie-Recommendation-",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Medcinal Plant Detection and Classification System",
+      description:
+        "A powerful and intelligent AI/ML-based application designed to detect and classify medicinal plants with high accuracy. Using Convolutional Neural Networks (CNN), the system analyzes plant images, identifies species, and provides relevant information about their medicinal properties. This project demonstrates the potential of deep learning in healthcare and agriculture, offering a fast and reliable solution for plant identification and research.",
+      image: medicinalplantLogo,
+      tags: ["Roboflow",  "API", "HTML", "CSS", "JavaScript", "Python", "TensorFlow", "Keras"],
+      github: "https://github.com/adi00005/Medicinal-plant-identification-and-classification",
+      // webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },
+    // {
+    //   id: 4,
+    //   title: "Task Reminder Chrome Extension Tool",
+    //   description:
+    //     "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+    //   image: taskremLogo,
+    //   tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    //   github: "https://github.com/codingmastr/Task-Reminder-Tool",
+    //   webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Webverse Digital",
+    //   description:
+    //     "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+    //   image: webverLogo,
+    //   tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+    //   github: "https://github.com/codingmastr/Webverse-Digital",
+    //   webapp: "https://webversedigital.com/",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   image: cmLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    //   github: "https://codingmasterweb.in/",
+    //   webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
